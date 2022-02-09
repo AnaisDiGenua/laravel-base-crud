84de8733608d9411f23daa21247b76f969313210
AnaisDiGenua <anais.digenua5@gmail.com>
2022-02-09 17:23:47 +0100

    fix migration table

--- database/migrations/2022_02_09_161608_create_comics_table.php
+++ database/migrations/2022_02_09_161608_create_comics_table.php
@@ -13,7 +13,7 @@ class CreateComicsTable extends Migration
      */
     public function up()
     {
-        Schema::create('_comics', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string("title", 50);
             $table->text("description");
@@ -33,6 +33,6 @@ class CreateComicsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_comics');
+        Schema::dropIfExists('comics');
     }
 }
